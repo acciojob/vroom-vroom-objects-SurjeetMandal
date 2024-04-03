403362767d5cc11a697deafbc02fb6b96872b0b5
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-03 11:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function Car (make, model) {
-	this.make = make;
-	this.model = model;
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
 }
-Car.prototype.getMakeModel = function(){
+Car.prototype.getMakeModel=function(){
 	return `${this.make} ${this.model}`;
 };
 
@@ -11,12 +11,13 @@ function SportsCar(make, model, topSpeed) {
 	this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
 
-SportsCar.prototype.getTopSpeed = function(){
+SportsCar.prototype.getTopSpeed=function(){
 	return this.topSpeed;
 }
 
 window.Car=Car;
+window.SportsCar=SportsCar;
 window.SportsCar=SportsCar;
