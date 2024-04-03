92eccee543b7418e51995cf95318cfe86258ecb1
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-03 11:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function Car(make,model){
-	this.make=make;
-	this.model=model;
+// Constructor function for Car
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 }
-Car.prototype.getMakeModel=function(){
-	return this.make+' '+this.model;
+
+// Method added to Car prototype
+Car.prototype.getMakeModel = function() {
+  return this.make + ' ' + this.model;
 };
 
-function SportsCar(make,model,topSpeed){
-	Car.call{this,make,model};
-	this.topSpeed = topSpeed;
+// Constructor function for SportsCar inheriting from Car
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // Call the Car constructor to inherit make and model
+  this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
+// Inherit Car prototype methods
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
-}
+// Method added to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 window.Car=Car;
 window.SportsCar=SportsCar;
